aa82e6cebfec0bde9b08b6c06549b37b1438c8f1
Ermekmz <mamazhanovermek1@gmail.com>
2023-10-05 17:59:33 +0600

    hw4

--- product/serializers.py
+++ product/serializers.py
@@ -4,18 +4,29 @@ from product.models import Product, Review, Category
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=100)
+    description = serializers.CharField(max_length=300)
+    price = serializers.IntegerField()
+    category = serializers.IntegerField()
+
     class Meta:
         model = Product
         fields = "__all__"
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    text = serializers.CharField(max_length=300)
+    product = serializers.IntegerField()
+    stars = serializers.IntegerField(min_value=1, max_value=5)
+
     class Meta:
         model = Review
         fields = "__all__"
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    name = serializers.CharField(max_length=100)
+
     class Meta:
         model = Category
         fields = "__all__"
